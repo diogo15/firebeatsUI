924c1389dc641154db406ba2a8fe3ac0899c0ee3
Ricardo <riaravi.araya.ra@gmail.com>
2022-11-16 00:16:28 -0600

    login almost ready,header ready,button component ready,and index component created ready to work

--- firebeats/src/app/app.module.ts
+++ firebeats/src/app/app.module.ts
@@ -10,12 +10,17 @@ import { PlaylistComponent } from './components/playlist/playlist.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SongUploadComponent } from './components/song-upload/song-upload.component';
-//Angular material API
+//Angular material library
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { ButtonComponent } from './components/button/button.component';
+import { IndexComponent } from './pages/index/index.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule }  from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -25,6 +30,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     HomePageComponent,
     LoginPageComponent,
     SongUploadComponent,
+    ButtonComponent,
+    IndexComponent,
+    HeaderComponent,
     
   ],
   imports: [
@@ -37,7 +45,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatButtonModule,
     MatProgressSpinnerModule,
     MatFormFieldModule,
-    
+    MatSnackBarModule,
+    MatSnackBarModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
