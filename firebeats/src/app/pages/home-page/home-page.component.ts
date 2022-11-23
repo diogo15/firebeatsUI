3034948bf130db487ae99703df6c233d581ffefc
Inm0rt4LKoff33 <fabimv23@gmail.com>
2022-11-23 01:04:30 -0600

    more css to the songs-component

--- firebeats/src/app/pages/home-page/home-page.component.ts
+++ firebeats/src/app/pages/home-page/home-page.component.ts
@@ -11,6 +11,18 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let tabs = document.getElementById('tabs')
+    var tabsItems = document.getElementsByClassName('tab')
+
+    for (let i = 0; i < tabsItems.length; i++) {
+      tabsItems[i].addEventListener('click', function () {
+        var current = document.getElementsByClassName('active')
+        current[0].className = current[0].className.replace(' active', '')
+
+        tabsItems[i].className += ' active'
+      })
+      
+    }
   }
 
 }
