cd0a3c4255274365f074920ba0c1860ef0608888
Diogo15 <luisdiegore@hotmail.com>
2022-12-12 00:59:15 -0600

    Pagina Registrar

--- firebeats/src/app/pages/register/register.component.ts
+++ firebeats/src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.sass']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  form : FormGroup;
+  constructor(private fb:FormBuilder) {
+    this.form = this.fb.group({
+      UserName:[''],
+      UserPassword:['']
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  registrar(){
+
   }
 
 }
